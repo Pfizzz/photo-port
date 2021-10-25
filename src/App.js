69d28a73aeab5708f9c63522c334988b631ac41a
Pfizz <pfiste80@gmail.com>
2021-10-25 19:33:29 -0400

    start of 3.4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Gallery from './components/Gallery';
 
 // import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Nav></Nav>
       <main>
+        <Gallery></Gallery>
         <About></About>
       </main>
       
